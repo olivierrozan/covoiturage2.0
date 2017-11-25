dc02072d791653810af5d4ec67eeb1100074d8b5
olivier <rozan.olivier@gmail.com>
2017-11-26 00:07:36 +0100

    Envoi de mail: simulation ok

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 export let allRoutes = (app, passport, urlencodedParser) => {
     // app.post("/connexion", urlencodedParser, (req, res, next) => {
     //     res.header("Access-Control-Allow-Origin", "*");
@@ -30,13 +32,57 @@ export let allRoutes = (app, passport, urlencodedParser) => {
         console.log("body parsing", req.body);
 
         passport.authenticate('local-signup', function (err, user) {
-            if (err) { 
-                return next(err); 
+            if (err) {
+                return next(err);
             }
-            if (!user) { 
-                return res.json({ message: 'Duplicate' }); 
+            if (!user) {
+                return res.json({ message: 'Duplicate' });
             } else {
-                return res.json({ message: 'Created' }); 
+                nodemailer.createTestAccount((err, account) => {
+
+                    // create reusable transporter object using the default SMTP transport
+                    let transporter = nodemailer.createTransport({
+                        host: account.smtp.host,
+                        port: account.smtp.port,
+                        secure: account.smtp.secure,
+                        auth: {
+                            user: account.user,
+                            pass: account.pass
+                        },
+                        logger: false,
+                        debug: false // include SMTP traffic in the logs
+                    });
+
+                    // setup email data with unicode symbols
+                    let mailOptions = {
+                        from: '"No reply" <noreply@covoit.fr>', // sender address
+                        to: '"Olivier Rozan" <rozan.olivier@gmail.com>', // list of receivers
+                        subject: 'Inscription réussie', // Subject line
+                        text: "Félicitations ! Vous êtes mantenant inscrit. Cliquez sur le lien suivant pour accéder à l'application", // plain text body
+                        html: "<h3>Félicitations !<h3>" + 
+                        "<div>Vous êtes mantenant inscrit.</div>" +
+                        "<div>Cliquez sur le lien suivant pour accéder à l'application:</div>"
+                    };
+
+                    // send mail with defined transport object
+                    transporter.sendMail(mailOptions, (error, info) => {
+                        if (error) {
+                            console.log('error sending mail');
+                            console.log(error.message);
+                            return process.exit(1);
+                        }
+                        console.log('Message sent: %s', info.messageId);
+                        // Preview only available when sending through an Ethereal account
+                        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                        // only needed when using pooled connections
+                        transporter.close();
+                        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+                        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+                    });
+                });
+
+                return res.json({ message: 'Created' });
             }
         })(req, res, next);
     });
