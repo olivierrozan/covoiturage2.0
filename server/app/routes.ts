c9f1648e9b753d71c6c130ea6917db1db418cd70
olivier <rozan.olivier@gmail.com>
2017-12-20 02:09:33 +0100

    Opti: Connexion dans un fichier à part

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt-nodejs');
 const headers = require('../config/header');
 const myOffers = require('./myOffers.ts');
 const register = require('./register.ts');
+const signin = require('./signin.ts');
 
 export let allRoutes = (app, passport, urlencodedParser, seq, Sequelize) => {
     
@@ -12,39 +13,7 @@ export let allRoutes = (app, passport, urlencodedParser, seq, Sequelize) => {
     };
 
     register.postRegister(app, seq, Sequelize, passport);
-    
-
-    app.post('/connexion', (req, res, next) => {
-        headers.setHeaders(res);
-        
-        try {
-            req.body = JSON.parse(Object.keys(req.body)[0]);
-        } catch (err) {
-            req.body = req.body;
-        }
-        console.log("body parsing", req.body);
-
-        passport.authenticate('local-signin', (err, user) => {
-            if (user) {
-                console.log('--user--', user.id);
-
-                req.logIn(user, function (err) {
-                    if (err) { return next(err); }
-                    return res.json({ message: 'success', user: user });
-                });
-            }
-        })(req, res, next);
-    });
-
-    app.get('/logout', (req, res) => {
-        headers.setHeaders(res);
-        
-        req.session.destroy((err) => {
-            console.log("BYE");
-            //res.clearCookie('connect.sid');
-            res.redirect('http://localhost:9200/#!/register');
-        });
-    });
+    signin.postSignin(app, seq, Sequelize, passport);
 
     app.get("/profile"/*, isLoggedIn*/, (req, res) => {
         
--- server/app/signin.ts
+++ server/app/signin.ts
@@ -1,5 +1,36 @@
 const headers = require('../config/header');
 
-export let getMyOffers = (app, seq, Sequelize) => {
+export let postSignin = (app, seq, Sequelize, passport) => {
     
+    app.post('/connexion', (req, res, next) => {
+        headers.setHeaders(res);
+        
+        try {
+            req.body = JSON.parse(Object.keys(req.body)[0]);
+        } catch (err) {
+            req.body = req.body;
+        }
+        console.log("body parsing", req.body);
+
+        passport.authenticate('local-signin', (err, user) => {
+            if (user) {
+                console.log('--user--', user.id);
+
+                req.logIn(user, function (err) {
+                    if (err) { return next(err); }
+                    return res.json({ message: 'success', user: user });
+                });
+            }
+        })(req, res, next);
+    });
+
+    app.get('/logout', (req, res) => {
+        headers.setHeaders(res);
+        
+        req.session.destroy((err) => {
+            console.log("BYE");
+            //res.clearCookie('connect.sid');
+            res.redirect('http://localhost:9200/#!/register');
+        });
+    });
 }
